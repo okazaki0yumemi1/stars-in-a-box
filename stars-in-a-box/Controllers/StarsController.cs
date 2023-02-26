d6bfc35e08fcc4688546aeb1f3b0aa4af1990aa1
Dmitry Melnikov <suwako.moria@yandex.ru>
2023-02-26 14:17:25 +0300

    Add View for Star/Details/id and NotFound

--- stars-in-a-box/Controllers/StarsController.cs
+++ stars-in-a-box/Controllers/StarsController.cs
@@ -2,6 +2,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace stars_in_a_box;
 public class StarsController : Controller
 {
+    private readonly DatabaseConnection _connection;
+
+    public StarsController(DatabaseConnection connection)
+    {
+        _connection = connection;
+    }
     // 
     // GET: /Stars/
     public IActionResult Index()
@@ -9,9 +15,14 @@ public class StarsController : Controller
         return View();
     }
     // 
-    // GET: /Stars/Welcome/ 
-    public string Welcome()
+    // GET: /Stars/Details/id
+    public async Task<IActionResult> Details(int id)
     {
-        return "This is the Welcome action method...";
+        var star = await _connection.Stars.FindAsync(id);
+        if (star == null) 
+        {
+            return NotFound();
+        }
+        return View(star);
     }
 }
